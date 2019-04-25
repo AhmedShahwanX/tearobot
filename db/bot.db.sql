a73a6f865d34a33bfe74c65e77ead476288189c4
ramysaafan <ramy224695@eng.zu.edu.eg>
2019-04-25 10:07:15 +0200

    modify db

--- db/bot.db.sql
+++ db/bot.db.sql
@@ -1,14 +1,19 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "Message" (
+CREATE TABLE
+IF NOT EXISTS "Message"
+(
 	"id"	INTEGER NOT NULL UNIQUE,
 	"update_id"	INTEGER NOT NULL UNIQUE,
 	"user_id"	INTEGER NOT NULL,
 	"chat_id"	INTEGER NOT NULL,
 	"date"	INTEGER NOT NULL,
 	"text"	TEXT,
-	PRIMARY KEY("id")
+	PRIMARY KEY
+("id")
 );
-CREATE TABLE IF NOT EXISTS "User" (
+CREATE TABLE
+IF NOT EXISTS "User"
+(
 	"id"	INTEGER NOT NULL UNIQUE,
 	"is_bot"	INTEGER NOT NULL DEFAULT 0,
 	"is_admin"	INTEGER NOT NULL DEFAULT 0,
@@ -22,4 +27,16 @@ CREATE TABLE IF NOT EXISTS "User" (
 	"last_command"	TEXT,
 	PRIMARY KEY("id")
 );
+
+CREATE TABLE
+IF NOT EXISTS "Everyday"
+(
+	"Day"	TEXT NOT NULL UNIQUE,
+	"first"	TEXT ,
+	"Second"	TEXT ,
+	"Break"	TEXT ,
+	"third"		TEXT ,
+	PRIMARY KEY("Day")
+);
+
 COMMIT;
