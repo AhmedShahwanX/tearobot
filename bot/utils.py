54841b2d4483320bb020c0b2ea3a23cfdc32f9e5
hazemgabr232 <hazemgabr232@gmail.com>
2019-04-28 02:43:37 +0200

    add is_admin & get_num_of_args

--- bot/utils.py
+++ bot/utils.py
@@ -1,13 +1,15 @@
 """
     Helper functions to be used inside the project
 """
-from .commands import start_command, help_command, weather, translate, calculate, tweet, ocr_url, events, stop
+from .commands import start_command, help_command, weather, translate, calculate, tweet, ocr_url, events, stop, \
+    new_announcement
 
 
 def is_available_command(command):
     """Checks if ``command`` is available in TBot commands"""
     available_commands = [
-        "/start", "/help", "/weather", "/translate", "/calculate", "/tweet", "/ocr_url", "/events", "/stop"]
+        "/start", "/help", "/weather", "/translate", "/calculate", "/tweet", "/ocr_url", "/events", "/stop",
+        "/new_announcement"]
 
     if command in available_commands:
         return True
@@ -16,7 +18,7 @@ def is_available_command(command):
 
 def command_takes_input(command):
     """Checks if ``command`` operates on inputs or not"""
-    takes_input = ["/translate", "/calculate", "/tweet", "/ocr_url"]
+    takes_input = ["/translate", "/calculate", "/tweet", "/ocr_url", "/new_announcement"]
     if command in takes_input:
         return True
     return False
@@ -33,7 +35,8 @@ def get_hint_message(command):
         "/tweet": "Let's tweet on TBot's twitter account!",
         "/ocr_url":   "Send the URL of the image you want to extract text from",
         "/stop": "",
-        "/events": ""
+        "/events": "",
+        "/new_announcement": "Write announcement id, time and description"
 
     }
     return commands_hint.get(command)
@@ -50,6 +53,26 @@ def get_command_handler(command):
         "/tweet": tweet,
         "/ocr_url": ocr_url,
         "/events": events,
-        "/stop": stop
+        "/stop": stop,
+        "/new_announcement": new_announcement
     }
     return command_service.get(command)
+
+
+def is_admin_command(command):
+    """Checks if ``command`` is admin commands"""
+    admin_commands = ["/new_announcement"]
+    if command in admin_commands:
+        return True
+    return False
+
+
+def get_num_of_args(command):
+    command_args = {
+        "/translate": 1,
+        "/calculate": 1,
+        "/tweet": 1,
+        "/ocr_url": 1,
+        "/new_announcement": 3
+    }
+    return command_args.get(command)
